d975004dd6e7a08c0db32af8ff3d2f38a848ec7b
fabious <fabious@free.fr>
2022-05-16 19:27:56 +0200

    remove arch config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,13 +3,6 @@
 # Globals
 set -gx EDITOR nvim
 
-# Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
-end
-
 # iTerm shell integration
 test -e ~/.iterm2_shell_integration.fish; and source ~/.iterm2_shell_integration.fish
 
@@ -19,5 +12,3 @@ test -e ~/.iterm2_shell_integration.fish; and source ~/.iterm2_shell_integration
 # It will replace the current version without touching Starship's configuration.
 starship init fish | source
 
-# asdf
-source /opt/asdf-vm/asdf.fish
