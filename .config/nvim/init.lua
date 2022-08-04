eebe571eec51961be40fd93cc89a2a5ed470a42b
Fabious <fabien@les-tilleuls.coop>
2022-08-04 14:09:52 +0200

    open diagnostic float keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -329,7 +329,7 @@ require('nvim-treesitter.configs').setup {
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist)
 
 -- LSP settings.
