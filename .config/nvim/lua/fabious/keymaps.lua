b27e9b549760ff3b0c1cbb3e01f6a819d3a56453
Fabious <fabious@free.fr>
2022-08-27 10:36:18 +0200

    avoid crash on fresh install

--- .config/nvim/lua/fabious/keymaps.lua
+++ .config/nvim/lua/fabious/keymaps.lua
@@ -88,20 +88,3 @@ map('n', '[g', '&diff ? "[g" : "<cmd>Gitsigns prev_hunk<CR>"', { expr = true })
 -- Zen mode
 map('n', '<leader>z', '<cmd>ZenMode<CR>')
 
--- Telescope
--- See `:help telescope.builtin`
-map('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-map('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-map('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer]' })
-map('n', '<C-p>', require('telescope.builtin').git_files, { desc = 'Fuzzy finder' })
-map('n', '<C-g>', require('telescope.builtin').live_grep, { desc = 'Live [G]rep' })
-map('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
-map('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
-map('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
-map('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
--- .config/nvim/lua/fabious/telescope.lua
+++ .config/nvim/lua/fabious/telescope.lua
@@ -13,3 +13,21 @@ require('telescope').setup {
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
+
+-- Keymaps
+-- See `:help telescope.builtin`
+map('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+map('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+map('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+map('n', '<C-p>', require('telescope.builtin').git_files, { desc = 'Fuzzy finder' })
+map('n', '<C-g>', require('telescope.builtin').live_grep, { desc = 'Live [G]rep' })
+map('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+map('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
+map('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
+map('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
