4bb73f8c97c1cf9065eddb23ebcf8c2b906935a4
Fabious <fabien@les-tilleuls.coop>
2023-01-20 16:38:20 +0100

    remove hop plugin

--- .config/nvim/lua/fabious/packer.lua
+++ .config/nvim/lua/fabious/packer.lua
@@ -59,15 +59,6 @@ require('packer').startup(function(use)
   }
 
   use {
-    'phaazon/hop.nvim',
-    branch = 'v2', -- optional but strongly recommended
-    config = function()
-      -- you can configure Hop the way you like here; see :h hop-config
-      require('hop').setup()
-    end,
-  }
-
-  use {
     'windwp/nvim-autopairs',
     config = function()
       require('nvim-autopairs').setup()
