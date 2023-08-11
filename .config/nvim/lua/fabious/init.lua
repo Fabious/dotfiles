30dd415554d702d4858d575615e95f9f1f60f33f
Fabious <fabious@free.fr>
2023-08-11 15:18:40 +0200

    move config files

--- .config/nvim/lua/fabious/init.lua
+++ .config/nvim/lua/fabious/init.lua
@@ -1,16 +1,4 @@
-require('fabious.keymaps')
-require('fabious.options')
+require('fabious.config')
 require('fabious.lazy')
 require('fabious.lsp')
 require('fabious.lspsaga')
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
