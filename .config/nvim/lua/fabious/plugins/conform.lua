10dc86b4cdd3ef345cbe272ced703295a43c9c44
Fabious <fabious@free.fr>
2023-12-06 12:13:30 +0100

    add conform formatter plugin

---
+++ .config/nvim/lua/fabious/plugins/conform.lua
@@ -0,0 +1,12 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+    formatters_by_ft = {
+      html = { 'htmlbeautifier' },
+    },
+  },
+}
