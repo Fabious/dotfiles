51fdf67c3b1e8c31f2c64676f317b61a6a540c9b
Fabious <fabious@free.fr>
2024-07-23 16:48:24 +0200

    Neotest: back to the game! with jest support

---
+++ .config/nvim/lua/fabious/plugins/neotest.lua
@@ -0,0 +1,53 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-neotest/neotest-jest',
+  },
+  config = function()
+    require('neotest').setup({
+      adapters = {
+        require('neotest-jest')({
+          jestCommand = 'npm test --',
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        }),
+      },
+    })
+  end,
+  keys = {
+    {
+      '<leader>tt',
+      function()
+        require('neotest').run.run(vim.fn.expand('%'))
+      end,
+      desc = 'Run File',
+    },
+    {
+      '<leader>tT',
+      function()
+        require('neotest').run.run(vim.loop.cwd())
+      end,
+      desc = 'Run All Test Files',
+    },
+    {
+      '<leader>tr',
+      function()
+        require('neotest').run.run()
+      end,
+      desc = 'Run Nearest',
+    },
+    {
+      '<leader>ts',
+      function()
+        require('neotest').summary.toggle()
+      end,
+      desc = 'Toggle Summary',
+    },
+  },
+}
